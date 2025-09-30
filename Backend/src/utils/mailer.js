40ae2c92b3238818dc1ab6f937881b1ef637950f
Sumedh Sangle <sanglesumedh15@gmail.com>
2025-09-30 12:21:42 +0530

    Nodemailer integrated using outlook personal email

---
+++ Backend/src/utils/mailer.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+dotenv.config();
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.office365.com",
+  port: 587,
+  secure: false, // TLS
+  auth: {
+    user: process.env.COLLEGE_EMAIL, 
+    pass: process.env.COLLEGE_PASS, 
+  },
+});
+
+export const sendEmail = async ({ to, subject, text, html }) => {
+  try {
+    const info = await transporter.sendMail({
+      from: `"LC-ICEM" <${process.env.COLLEGE_EMAIL}>`,
+      to,
+      subject,
+      text,
+      html,
+    });
+    console.log("Email sent: ", info.messageId);
+    return info;
+  } catch (err) {
+    console.error("Error sending email: ", err);
+    throw err;
+  }
+};
